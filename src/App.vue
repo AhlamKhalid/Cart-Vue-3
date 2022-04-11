f33a6bf5ee251ad89908e25e4e415824d5705b96
AhlamKhalid <ahlamkarkashan@gmail.com>
2022-04-11 20:58:56 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,227 @@
+<script setup>
+// Reactivity, Computed, Lifecycle hook
+import { ref, computed, onMounted } from "vue";
+
+// local image
+// import makeupImage from "@/assets/img/makeup.jpeg";
+
+// components
+import Product from "@/components/Product.vue";
+
+// data
+const isCartOpen = ref(false);
+const products = ref([]);
+
+// computed
+const numCartItems = computed(() =>
+  products.value.reduce((numItems, product) => numItems + product.quantity, 0)
+);
+
+const cartTotal = computed(() =>
+  products.value.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  )
+);
+
+// lifecycle
+onMounted(async () => {
+  // products.value = [
+  //   {
+  //     id: 1,
+  //     img: makeupImage,
+  //     name: "Red lipstick",
+  //     price: 16,
+  //     quantity: 2
+  //   },
+  //   {
+  //     id: 2,
+  //     img: makeupImage,
+  //     name: "Red lipstick",
+  //     price: 16,
+  //     quantity: 1
+  //   },
+  //   {
+  //     id: 3,
+  //     img: makeupImage,
+  //     name: "Red lipstick",
+  //     price: 16,
+  //     quantity: 1
+  //   },
+  //   {
+  //     id: 4,
+  //     img: makeupImage,
+  //     name: "Red lipstick",
+  //     price: 16,
+  //     quantity: 1
+  //   }
+  // ];
+  products.value = await fetchProducts();
+});
+
+// methods
+const showCart = () => {
+  isCartOpen.value = true;
+};
+
+const hideCart = () => {
+  isCartOpen.value = false;
+};
+
+/**** Sync ****/
+// const removeItem = ($event) => {
+//   const productId = $event;
+
+//   products.value = products.value.filter((product) => product.id !== productId);
+// };
+
+/**** Async ****/
+const removeItem = async ($event) => {
+  const productId = $event;
+
+  const response = await fetch(`http://localhost:5000/products/${productId}`, {
+    method: "DELETE"
+  });
+
+  products.value = products.value.filter((product) => product.id !== productId);
+};
+
+/**** Sync ****/
+// const decrementQuantity = ($event) => {
+//   const productId = $event;
+
+//   const productIndex = products.value.findIndex(
+//     (product) => product.id === productId
+//   );
+
+//   // if quantity is 1, remove item
+//   if (products.value[productIndex].quantity === 1) removeItem(productId);
+//   else products.value[productIndex].quantity--;
+// };
+
+/**** Async ****/
+const decrementQuantity = async ($event) => {
+  const productId = $event;
+  const productIndex = products.value.findIndex(
+    (product) => product.id === productId
+  );
+
+  // if quantity is 1, remove item
+  if (products.value[productIndex].quantity === 1) removeItem(productId);
+  else {
+    const productToUpdate = await fetchProduct(productId);
+    const updatedProduct = {
+      ...productToUpdate,
+      quantity: --productToUpdate.quantity
+    };
+
+    const response = await fetch(
+      `http://localhost:5000/products/${productId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(updatedProduct)
+      }
+    );
+
+    products.value[productIndex].quantity = updatedProduct.quantity;
+  }
+};
+
+/**** Sync ****/
+// const incrementQuantity = ($event) => {
+//   const productId = $event;
+
+//   const productIndex = products.value.findIndex(
+//     (product) => product.id === productId
+//   );
+
+//   products.value[productIndex].quantity++;
+// };
+
+/**** Async ****/
+const incrementQuantity = async ($event) => {
+  const productId = $event;
+  const productIndex = products.value.findIndex(
+    (product) => product.id === productId
+  );
+
+  const productToUpdate = await fetchProduct(productId);
+  const updatedProduct = {
+    ...productToUpdate,
+    quantity: ++productToUpdate.quantity
+  };
+
+  const response = await fetch(`http://localhost:5000/products/${productId}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify(updatedProduct)
+  });
+
+  products.value[productIndex].quantity = updatedProduct.quantity;
+};
+
+const fetchProducts = async () => {
+  const response = await fetch("http://localhost:5000/products");
+  const data = await response.json();
+  return data;
+};
+
+const fetchProduct = async (productId) => {
+  const response = await fetch(`http://localhost:5000/products/${productId}`);
+  const data = await response.json();
+  return data;
+};
+</script>
+
+<template>
+  <!-- cart -->
+  <button id="cart-btn" @click="showCart">
+    <i class="fa-brands fa-opencart fa-xl"></i>
+  </button>
+  <!-- cart sidebar -->
+  <div id="cart-sidebar" class="cart-sidebar" :class="{ show: isCartOpen }">
+    <!-- header -->
+    <div class="cart-header-padding">
+      <div class="cart-header">
+        <p>
+          <span class="title">cart</span>
+          <span class="items">
+            (<span>{{ numCartItems }}</span>
+            {{ numCartItems === 1 ? "item" : "items" }})</span
+          >
+        </p>
+        <button id="close-btn" @click="hideCart">
+          <i class="fa-solid fa-xmark fa-xl"></i>
+        </button>
+      </div>
+    </div>
+    <!-- products -->
+    <div class="cart-products">
+      <Product
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+        @remove-item="removeItem"
+        @decrement-quantity="decrementQuantity"
+        @increment-quantity="incrementQuantity"
+      />
+    </div>
+    <!-- footer -->
+    <div class="cart-footer">
+      <div class="total">
+        <p>total</p>
+        <p>${{ cartTotal }}</p>
+      </div>
+      <a href="#" class="checkout-link">checkout</a>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "@/assets/css/base.css";
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,98 @@
+<script setup>
+defineProps(["product"]);
+defineEmits(["remove-item", "decrement-quantity", "increment-quantity"]);
+</script>
+
+<template>
+  <div class="product">
+    <div class="details">
+      <img :src="product.img" :alt="product.name" class="product-img" />
+      <div class="product-info">
+        <p class="product-name">{{ product.name }}</p>
+        <p class="product-price">${{ product.price }}</p>
+      </div>
+    </div>
+    <div class="controls">
+      <div class="quantity">
+        <button
+          class="decrement-btn"
+          @click="$emit('decrement-quantity', product.id)"
+        >
+          <i class="fa-solid fa-minus fa-sm"></i>
+        </button>
+        <p class="quantity-num">{{ product.quantity }}</p>
+        <button
+          class="increment-btn"
+          @click="$emit('increment-quantity', product.id)"
+        >
+          <i class="fa-solid fa-plus fa-sm"></i>
+        </button>
+      </div>
+      <button class="trash-btn" @click="$emit('remove-item', product.id)">
+        <i class="fa-solid fa-trash-can fa-lg"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.product {
+  margin-bottom: 10px;
+  border: 1px solid #e8e8e8;
+  border-radius: 3px;
+}
+
+.details {
+  padding: 15px;
+  display: flex;
+  align-items: center;
+}
+
+.product-img {
+  width: 100px;
+}
+
+.product-info {
+  margin-left: 20px;
+}
+
+.product-name {
+  font-size: 12px;
+  font-weight: 700;
+}
+
+.product-price {
+  font-size: 12px;
+  margin-top: 4px;
+}
+
+/* controls */
+.controls {
+  border-top: 1px solid #e8e8e8;
+  display: flex;
+}
+
+.quantity,
+.trash-btn {
+  width: 50%;
+}
+
+.quantity {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 15px;
+}
+
+.quantity-num {
+  font-size: 12px;
+}
+
+.trash-btn {
+  border-left: 1px solid #e8e8e8;
+}
+
+.trash-btn:hover {
+  background-color: #f63a35;
+  color: white;
+}
+</style>
